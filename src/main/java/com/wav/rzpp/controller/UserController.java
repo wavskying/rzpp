5659a19089f63dd04d9baa070f1885b0b613b76d
wavskying <2817253626@qq.com>
2023-02-10 22:16:17 +0800

    测试提交

--- src/main/java/com/wav/rzpp/controller/UserController.java
+++ src/main/java/com/wav/rzpp/controller/UserController.java
@@ -45,6 +45,7 @@ public class UserController {
     @ExcludeJwtVerify
     @PostMapping("/login")
     public AjaxResult login(RzUser rzUser) throws ValidatorException {
+        System.out.println("111");
         ValidatorUtil.checkLogin(rzUser.getUsername(), rzUser.getPassword());
         RzUser result = rzUserService.isLogin(rzUser.getUsername(), rzUser.getPassword());
         if (result != null) {
